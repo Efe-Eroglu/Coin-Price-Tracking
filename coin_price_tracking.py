4a58b01ebcc9c08725ec2a8f871809a1552b782d
efe <eferoglu1967@gmail.com>
2023-06-05 18:35:10 +0300

    first commit

---
+++ coin_price_tracking.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import pyautogui
+
+
+def menu():
+
+    print("Which currency chart to follow ?\n1-) BİTCOİN / U.S. DOLLAR\n2-) ETHEREUM / TETHERUS\n3-) BİTCOİN / TETHERUS\n4-) EURO FX/ U.S. DOLLAR")
+    choice=input()
+    
+    coin_link={
+
+        "1":"https://tr.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD",
+        "2":"https://tr.tradingview.com/chart/?symbol=BINANCE%3AETHUSDT",
+        "3":"https://tr.tradingview.com/chart/?symbol=BINANCE%3ABTCUSDT",
+        "4":"https://tr.tradingview.com/chart/?symbol=FX%3AEURUSD"
+    }
+
+    return coin_link[choice]
+
+
+def main(link):
+
+    option=input("Would you like to receive notifications via Whatsapp message? ? (y/n)\n")
+
+    if option=="y":
+        wp_driver=webdriver.Chrome(ChromeDriverManager().install())
+        wp_driver.set_window_position(500, 10)
+        wp_driver.get("https://web.whatsapp.com/")
+        input("\n--* Select the person to be notified on WhatsApp and press enter to continue.. *--\n")
+    
+    elif option == "n":
+        print("\nContinues without whatsapp notification.\n")
+        
+    else:
+        print("\nAnswer only one letter (y/n).\n")
+
+
+    driverOptions = webdriver.ChromeOptions()
+    driverOptions.add_argument("--incognito")
+    driverOptions.add_argument("--headless")
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=driverOptions)
+    driver.get(link)
+
+    WebDriverWait(driver,10).until(ec.visibility_of_element_located((By.XPATH,"/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]")))
+
+    coin_name=driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/span[2]").text
+    currency=driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[2]/span[2]").text
+    first_price=driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]").text
+
+    while True:
+        try:
+            price_text=driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]").text
+            print(coin_name+" =>",price_text,currency)
+            
+            if price_text>first_price:
+                first_price=price_text
+                print("-- Currency value increased --")
+
+                if option=="y":
+                    wp_driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p").click()
+                    pyautogui.typewrite("-- Currency value increased --")
+                    pyautogui.press("Enter")
+
+            sleep(5)
+
+        except Exception:
+            driver.quit()
+            print("Something went wrong")
+
+main(menu())
